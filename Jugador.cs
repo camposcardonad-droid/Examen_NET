99a38fa728787d6cb3aecd7d54c3c09aaffb6e6f
Dominic Campos <camposcardonad@gmail.com>
2026-09-11 17:16:18 -0600

    error al subir Jugador - Reintento

--- Jugador.cs
+++ Jugador.cs
@@ -5,6 +5,41 @@ using System.Text;
 namespace ExamenRuleta
 {
     internal class Jugador
-    {
+    { 
+        public decimal Dinero {  get; set; }
+        private List<Giro> historial;
+
+        public Jugador()
+        {
+            Dinero = 300;
+            historial = new List<Giro>();
+        }
+        public void RestarDinero(decimal cantidad)
+        {
+            Dinero = Dinero - cantidad;
+        }
+        public void AgregarDinero(decimal cantidad)
+        {
+            Dinero = Dinero + cantidad;
+        }
+        public void AgregarGiro(Giro giro)
+        {
+            historial.Add(giro);
+        }
+        public void MostrarHistorial()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Historial");
+            Console.WriteLine("=========");
+            if (historial.Count == 0)
+            {
+                Console.WriteLine("Todavia no hay giros");
+                return;
+            }
+            for (int i = 0; i < historial.Count; i++ )
+            {
+                Console.WriteLine((i + 1) + ". " + historial[i]);
+            }
+        }
     }
 }
