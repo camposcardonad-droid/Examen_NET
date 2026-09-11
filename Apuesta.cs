3cadbf4882ec2c0d2f7e1c86b70681d9e12fa3bb
Dominic Campos <camposcardonad@gmail.com>
2026-09-10 18:24:38 -0600

    Funcionamiento de Apuesta

--- Apuesta.cs
+++ Apuesta.cs
@@ -6,5 +6,43 @@ namespace ExamenRuleta
 {
     internal class Apuesta
     {
+        public Apuesta(decimal cantidad)
+        {
+            Cantidad = cantidad;
+        }
+
+        public decimal Cantidad { get; set;  }
+        public bool EsValida(decimal cantidadActual)
+        {
+            //cantidadActual = cantidad de dinero que tenemos disponible
+            if (Cantidad == 0)
+            {
+                return false;
+            }
+            if (Cantidad % 10 != 0)
+            {
+                return false;
+            }
+            if (Cantidad > cantidadActual)
+            {
+                return false;
+            }
+            return true;
+        }
+        public decimal ApuestaNumero()
+        {
+            decimal resultado = Cantidad * 10;
+            return resultado;
+        }
+        public decimal ApuestaColor()
+        {
+            decimal resultado = Cantidad * 5;
+            return resultado;
+        }
+        public decimal ApuestaPar()
+        {
+            decimal resultado = Cantidad * 2;
+            return resultado;
+        }
     }
 }
